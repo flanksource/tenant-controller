d8ccff3fd27e67110ba1931afa917b3b49147b22
Yash Mehrotra <yashmehrotra95@gmail.com>
2024-11-18 11:16:35 +0530

    fix: gcp empty secret handling

--- pkg/secrets/gcp.go
+++ pkg/secrets/gcp.go
@@ -4,5 +4,5 @@ type GCPSealedSecret struct{}
 
 func (s GCPSealedSecret) GenerateSealedSecret(params SealedSecretParams) ([]byte, error) {
 	// We setup SQLUser and database via CRDs
-	return nil, nil
+	return []byte(""), nil
 }

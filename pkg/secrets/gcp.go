a2985e5d3a80792be0820568adc2771eb8e9107b
Yash Mehrotra <yashmehrotra95@gmail.com>
2024-11-18 13:59:34 +0530

    chore: add sops encryption for gcp

--- pkg/secrets/gcp.go
+++ pkg/secrets/gcp.go
@@ -1,8 +1,26 @@
 package secrets
 
+import (
+	"os/exec"
+
+	v1 "github.com/flanksource/tenant-controller/api/v1"
+)
+
 type GCPSealedSecret struct{}
 
 func (s GCPSealedSecret) GenerateSealedSecret(params SealedSecretParams) ([]byte, error) {
-	// We setup SQLUser and database via CRDs
-	return []byte(""), nil
+	fileName, err := createDBSecretFile(params.Namespace, params.Username, params.Password)
+	if err != nil {
+		return nil, err
+	}
+
+	cmd := exec.Command(
+		"sops", "--encrypt",
+		"--encrypted-regex", "stringData",
+		"--gcp-kms", v1.GlobalConfig.GCP.KMS,
+		fileName,
+	)
+
+	return cmd.CombinedOutput()
+
 }
